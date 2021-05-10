5b1e8ce4f21af1906c2ac0f68d7bf557b99190eb
yuu <yuu.email@example.com>
2021-05-11 08:31:45 +0900

    サイドメニュー,ヘッダーの作成

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,19 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    drawer: false,
+  },
+  mutations: {
+    SideMenu(state) {
+      state.drawer = !state.drawer;
+    },
+  },
+  actions: {
+    SideMenu({ commit }) {
+      commit("SideMenu");
+    },
+  },
 });
